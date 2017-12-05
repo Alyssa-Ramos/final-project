f58c5f490a671235d31656c628268bdcdf3aa039
Alyssa-Ramos <31941449+Alyssa-Ramos@users.noreply.github.com>
2017-12-05 13:55:00 -0500

    Fixed DIV Map!

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,12 @@ $(document).ready(function() {
     hideAll(); */
 
 //Social Implication Functions//
-  function hideAll() {
+
    $(".socImp1").hide();
    $(".socImp2").hide();
    $(".socImp3").hide();
    $(".socImp4").hide();
-   }
-   hideAll();
+
 
   $('.socHead1').click(function() {
     $('.socImp1').slideToggle();
@@ -49,7 +48,19 @@ $('.environHead2').click(function() {
   $('.environImp2').slideToggle();
 });
 
+// function to remove and replace the map divided
+
+function manageMapDiv() {
+  $("#worldMap").remove();
+  $("#worldMapHolder").html("<div id='worldMap'></div>");
+}
+
+
+
 /* MENU SELECT FUNCTION */
+  // initialize the variable for all maps below
+  var mymap;
+
 
    $( "#menu" ).change(function() {
 
@@ -58,8 +69,10 @@ $('.environHead2').click(function() {
       case "BGD":
 
          // alert("Bangladesh");
+         // run the function to remove and remake the div
+        manageMapDiv();
 
-        var mymap = L.map('worldMap').setView([23.562489, 66.474645],6.00);
+        mymap = L.map('worldMap').setView([23.562489, 66.474645],6.00);
 
       L.tileLayer('https://api.mapbox.com/styles/v1/alyssa-ramos/cja3f9cd11m9g2smgswlty8bs/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWx5c3NhLXJhbW9zIiwiYSI6ImNqYTNleGg4djJuejgyd2xndTV1ZzhmNTUifQ.CkEJwd5T0BA3HPHovs_NTQ', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -76,8 +89,10 @@ $('.environHead2').click(function() {
 
         case "PAK":
           //Pakistan
+          // run the function to remove and remake the div
+         manageMapDiv();
 
-          var mymap = L.map('worldMap1').setView([23.562489, 66.474645],6.00);
+          mymap = L.map('worldMap').setView([23.562489, 66.474645],6.00);
 
           L.tileLayer('https://api.mapbox.com/styles/v1/alyssa-ramos/cjalju9fvby0t2rs6t9kcc4mo/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWx5c3NhLXJhbW9zIiwiYSI6ImNqYTNleGg4djJuejgyd2xndTV1ZzhmNTUifQ.CkEJwd5T0BA3HPHovs_NTQ', {
               attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
